f1626af3b7b07605e2155b4b56d73465d245b227
EWest80 <2019900+Ewest80@users.noreply.github.com>
2023-05-10 00:45:50 -0400

    Validations, password info text

--- js/script.js
+++ js/script.js
@@ -1,16 +1,34 @@
 const form = document.querySelector('.form');
 const password = document.getElementById('password');
 const confirmPassword = document.getElementById('confirm-password');
+const passwordError = document.querySelector('.password-error');
+const passwordReq = document.querySelector('.password-requirements');
 
 form.addEventListener('submit', (e) => {
   if(password.value !== confirmPassword.value) {
     e.preventDefault();
     password.classList.add('error');
     confirmPassword.classList.add('error');
+    passwordError.style.display = 'block';
   }  
 })
 
 password.addEventListener('focus', () => {
   password.classList.remove('error')
   confirmPassword.classList.remove('error');
+  passwordReq.style.display = 'block';
 });
+
+password.addEventListener('blur', () => {
+  passwordReq.style.display = 'none'
+  passwordError.style.display = 'none'
+});
+
+password.addEventListener('input', () => {
+  if (password.value.length < 8) {
+    passwordReq.style.color = 'red';
+  }
+  else {
+    passwordReq.style.color = 'green';
+  }
+})
